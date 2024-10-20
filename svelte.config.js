8947e5c83b6f87176ebab5103c46c918ef7cb0b0
Mark Burggraf <markb@mantisbible.com>
2024-10-20 16:00:22 -0700

    project configuration for shadcn-svelte, tailwind, svelte5: the magic is in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,34 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true,
+		}),
+	},
+	vitePlugin: {
+		inspector: true,
+		experimental: {
+			// This will suppress the warning
+			disableLegacyReactivityWarnings: true,
+		},
+	},
+	onwarn: (warning, handler) => {
+		if (warning.code === 'a11y-missing-attribute') return
+		// Suppress the specific warning about <slot> deprecation
+		if (warning.message.includes('Using `<slot>` to render parent content is deprecated')) return
+		if (warning.message.includes('Properties of objects and arrays are not reactive unless in runes mode')) return;
 
-export default config;
+		// Handle all other warnings
+		handler(warning)
+	},
+}
+
+export default config
